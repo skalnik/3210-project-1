072cbb8a61eb05960485f9602c43db89fb099367
Michael Peter Skalnik <mskalnik3@factor-3210.cc.gatech.edu>
2012-02-07 17:54:12 -0500

    Create /proc entry

--- lfprng.c
+++ lfprng.c
@@ -1,15 +1,15 @@
-#define __KERNEL__
-#define MODULE
-
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
-int init_module()
+int init_module(void)
 {
-  proc_file = create_proc_entry("lfprng", "666", proc_root);
+  struct proc_dir_entry* proc_file;
+  proc_file = create_proc_entry("lfprng", 0755, NULL);
   return 0;
 }
 void cleanup_module()
 {
-  proc_file = remove_proc_entry("lfprng", proc_root);
+  remove_proc_entry("lfprng", NULL);
 }
+
