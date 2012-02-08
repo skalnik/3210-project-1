6d890e267c0318c3e79b73c2cdcd55c2a4c444ca
Mike Skalnik <mike.skalnik@gmail.com>
2012-02-08 14:36:36 -0500

    Try to print PID upon read

--- lfprng.c
+++ lfprng.c
@@ -1,13 +1,31 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/tty.h>
 
 #define MODULE_NAME "Leap Frog Psuedo Random Number Generator"
 
+static struct proc_dir_entry *proc_file;
+
+/* Called when someone attempts to read a psuedo random number /proc/lfprng */
+int read_proc_lfprng(char *buffer,
+                     char **buffer_location,
+                     off_t offset,
+                     int buffer_length,
+                     int *eof,
+                     void* data) {
+  printk("PID is:%ld", (long)current->pid);
+  return 0;
+}
+
 int init_module(void)
 {
-  struct proc_dir_entry* proc_file;
   proc_file = create_proc_entry("lfprng", 0755, NULL);
+
+  proc_file->owner = THIS_MODULE;
+  proc_file->read_proc = read_proc_lfprng;
+
   return 0;
 }
 void cleanup_module()
