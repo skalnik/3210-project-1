ea2450fa82e926f2ed50b8dec92d2a1af048b81a
Nicholas Thomas Smith <nsmith9@factor-3210.cc.gatech.edu>
2012-02-09 12:06:15 -0500

    stubbed write_proc, threading framework, double checking calculations before implementation

--- lfprng.c
+++ lfprng.c
@@ -5,8 +5,10 @@
 #include <linux/tty.h>
 
 #define MODULE_NAME "Leap Frog Psuedo Random Number Generator"
+#define STACK 1024*64
 
 static struct proc_dir_entry *proc_file;
+
 
 /* Called when someone attempts to read a psuedo random number /proc/lfprng */
 int read_proc_lfprng(char *buffer,
@@ -18,6 +20,62 @@ int read_proc_lfprng(char *buffer,
   printk("PID is:%ld", (long)current->pid);
   return 0;
 }
+/*writes LFprn into /proc/lfprng*/
+int write_proc_lfprng(float psn)
+{
+  /*stub*/
+  printk("Out: %f", psn);
+  return 0;
+}
+
+/*spawns worker threads*/
+int create_lfprng(int seed, pid_t pid)
+{
+  /*t = pid -> threadCount*/
+  int t = 1
+  int seed_array[t]
+  void * stack = malloc(STACK)
+
+  for (i = 0, i < t, i++)
+  {
+    seed_array[i] = seed
+  }
+
+  if (stack == 0) {
+    perror("malloc: could not allocate stack");
+    exit(1);
+  }
+
+  for(i = 0, i < t, i++)
+  {  
+    pid = clone(&threadFunction,
+                 (char*) stack + STACK,
+                 SIGCHLD | CLONE_FS | CLONE_FILES |\
+		 CLONE_SIGHAND | CLONE_VM,
+                 (void*)fd); 
+ 
+    if (pid == -1) {
+      perror("clone");
+      exit(2);
+    } 
+ 
+  // Wait for the child thread to exit
+  pid = waitpid(pid, 0, 0);
+  if (pid == -1) {
+    perror("waitpid");
+    exit(3);
+  } 
+
+  free(stack)
+
+}
+
+/*worker thread function*/
+void calculate_lfprng(int seed, int thread_count)
+{
+
+}
+
 
 int init_module(void)
 {
