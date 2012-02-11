b980ae7c1e51e6f81d2703463b9be5ae8691544a
Nicholas Thomas Smith <nsmith9@factor-3210.cc.gatech.edu>
2012-02-11 01:27:35 -0500

    thread creation is almost done. still have a void * cast that is the only compilation issue

--- lfprng.c
+++ lfprng.c
@@ -2,13 +2,23 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
+#include <linux/kthread.h>
 #include <linux/tty.h>
+#include <linux/vmalloc.h>
+#include <linux/syscalls.h>
+#include <linux/sys.h>
+#include <linux/signal.h>
+
+
 
 #define MODULE_NAME "Leap Frog Psuedo Random Number Generator"
 #define STACK 1024*64
 
 static struct proc_dir_entry *proc_file;
 
+struct thread_data{
+  int seed,i,a,b,c;
+};
 
 /* Called when someone attempts to read a psuedo random number /proc/lfprng */
 int read_proc_lfprng(char *buffer,
@@ -28,53 +38,60 @@ int write_proc_lfprng(float psn)
   return 0;
 }
 
+ /*worker thread function*/
+void calculate_lfprng(void* thread_seed)
+{
+  struct thread_data *my_seed;
+  my_seed = (struct thread_data *) thread_seed;
+  printk("this is a thread %i",my_seed->a);
+}
+
 /*spawns worker threads*/
-int create_lfprng(int seed, pid_t pid)
+void create_lfprng(int seed, pid_t pid)
 {
   /*t = pid -> threadCount*/
-  int t = 1
-  int seed_array[t]
-  void * stack = malloc(STACK)
+  int t = 1;
+  int seed_array[t];
+  struct task_struct *ts;
 
-  for (i = 0, i < t, i++)
+  int i = 0;
+   /*not real values*/
+  int a = 2; 
+  int b = 0;
+  int c = 8;
+  struct thread_data array[t]; 
+  struct thread_data* input;  
+  for (i= 0; i < t; i++)
   {
-    seed_array[i] = seed
+    seed_array[i] = (seed * (( a^i ) % c));
   }
 
-  if (stack == 0) {
-    perror("malloc: could not allocate stack");
-    exit(1);
-  }
-
-  for(i = 0, i < t, i++)
+  for (i = 0;i < t; i++)
   {  
-    pid = clone(&threadFunction,
-                 (char*) stack + STACK,
-                 SIGCHLD | CLONE_FS | CLONE_FILES |\
-		 CLONE_SIGHAND | CLONE_VM,
-                 (void*)fd); 
+    int num = seed_array[i];
+    array[i].seed = num;
+    array[i].i = i;
+    array[i].a = a;
+    array[i].b = b;
+    array[i].c = c;
+    input = &array[i];
+    ts = kthread_run(calculate_lfprng, (void*) input, "caclfprn"); 
  
-    if (pid == -1) {
-      perror("clone");
-      exit(2);
+    if (pid == -1)
+    {
+      printk("kthread_run");
     } 
- 
+  }
+    
   // Wait for the child thread to exit
-  pid = waitpid(pid, 0, 0);
-  if (pid == -1) {
-    perror("waitpid");
-    exit(3);
-  } 
+    pid = sys_waitpid(pid, 0, 0);
 
-  free(stack)
-
+    if (pid == -1) {
+      printk("waitpid");
+    } 
 }
 
-/*worker thread function*/
-void calculate_lfprng(int seed, int thread_count)
-{
 
-}
 
 
 int init_module(void)
@@ -86,8 +103,10 @@ int init_module(void)
 
   return 0;
 }
-void cleanup_module()
+void cleanup_module(void) 
 {
   remove_proc_entry("lfprng", NULL);
 }
 
+
+		
